862a2816e844e3686c509eca76234efc2153a780
Mai Anh Tien <tienham1610@gmail.com>
2023-06-19 00:42:43 +0700

    WIP in 19-6-06-2023

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
     
     $('.feature-nav').each(function () {
-        
         $(this).on('click', function () {
-
             $('.feature-nav').removeClass('active')
             $(this).addClass('active');
         })
     })
+    
 });
